ee6ff9360d230a78f179086f884254c653232602
Neo <admin@wzs.app>
2026-07-18 10:35:13 +0800

    add param

--- Sources/Opus/include/OggOpusWriter.h
+++ Sources/Opus/include/OggOpusWriter.h
@@ -1,22 +1,36 @@
 #import <Foundation/Foundation.h>
+#import <opus/opus_defines.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @class DataItem;
 
+typedef NS_ENUM(NSInteger, OggOpusWriterApplication) {
+  OggOpusWriterApplicationVoip = OPUS_APPLICATION_VOIP,
+  OggOpusWriterApplicationAudio = OPUS_APPLICATION_AUDIO,
+};
+
 @interface OggOpusWriter : NSObject
 
-@property (nonatomic, assign) int inputSampleRate;
+- (instancetype)initWithInputSampleRate:(NSInteger)inputSampleRate
+                                bitrate:(NSInteger)bitrate;
+
+- (instancetype)initWithInputSampleRate:(NSInteger)inputSampleRate
+                                bitrate:(NSInteger)bitrate
+                            application:(OggOpusWriterApplication)application
+    NS_DESIGNATED_INITIALIZER;
 
 - (bool)beginWithDataItem:(DataItem *)dataItem;
 - (bool)beginAppendWithDataItem:(DataItem *)dataItem;
 
-- (bool)writeFrame:(uint8_t * _Nullable)framePcmBytes frameByteCount:(NSUInteger)frameByteCount;
+- (bool)writeFrame:(uint8_t *_Nullable)framePcmBytes
+    frameByteCount:(NSUInteger)frameByteCount;
 - (NSUInteger)encodedBytes;
 - (NSTimeInterval)encodedDuration;
 
 - (NSDictionary *)pause;
-- (bool)resumeWithDataItem:(DataItem *)dataItem encoderState:(NSDictionary *)state;
+- (bool)resumeWithDataItem:(DataItem *)dataItem
+              encoderState:(NSDictionary *)state;
 
 @end
 
